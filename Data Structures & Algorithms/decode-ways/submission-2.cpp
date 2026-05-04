ebe4088e8e3ed4feda844d7313435e9cbdfa11e6
akilsakthivelg <akilsakthivel2007@gmail.com>
2026-05-04 20:24:03 +0530

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int dp=0,dp1=1,dp2=0;
+        int n=s.size();
+        for (int i=n-1;i>=0;i--) {
+            if (s[i]=='0') dp=0;
+            else {
+                dp=dp1;
+                if (i+1<n && (s[i]=='1' || (s[i]=='2' && s[i+1]<='6'))) dp+=dp2;
+            }
+            dp2=dp1;
+            dp1=dp;
+            dp=0;
+        }
+        return dp1;
+    }
+};
