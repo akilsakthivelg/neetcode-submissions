0db63303e4b73d7355a70c6f674f88a053d6504a
AKIL SAKTHIVEL <akilsakthivel2007@gmail.com>
2026-07-15 13:54:46 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int z,o,t;
+        z=o=t=0;
+        for (auto x:nums) {
+            if (x==0) z++;
+            else if (x==1) o++;
+            else t++;
+        }
+        int i=0;
+        while (z--) nums[i++]=0;
+        while (o--) nums[i++]=1;
+        while (t--) nums[i++]=2;
+
+    }
+};
